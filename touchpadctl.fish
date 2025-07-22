9e4f81d4ff6f1652b77d1054e31ee0db2c2f3690
LiAlH4 <lialh4qwq@outlook.com>
2025-07-22 22:29:43 +0800

    fix: add desktop notification back

--- touchpadctl.fish
+++ touchpadctl.fish
@@ -3,27 +3,27 @@
 # touchpadctl
 # Author: LiAlH4
 
+set -g APP_NAME "touchpadctl" #: "touchpadctl"
+set -g APP_VERSION "v0.2.0" #: string
+set -g SETTING_PATH "org.gnome.desktop.peripherals.touchpad" "send-events" #: string[2]
+
 # Args
 # - subCommand: string
-function touchpadctl -a subCommand #: string
-    set -l appName "touchpadctl" #: "touchpadctl"
-    set -l appVersion "v0.2.0" #: string
+function touchpadctl -a subCommand #: void
     switch $subCommand
         case "enable" "disable" "toggle"
             changeTouchpadState $subCommand
         case "help"
-            showHelpMessage $appName $appVersion
+            showHelpMessage
         case "*"
-            showInvalidSubCommandError $appName $subCommand
+            showInvalidSubCommandError $subCommand
     end
 end
 
 # Args
-# - appName: "touchpadctl"
-# - appVersion: string
-function showHelpMessage -a appName appVersion #: string
-    printf "%s %s\n" $appName $appVersion
-    printf "Usage: %s <sub command>\n" $appName
+function showHelpMessage #: void
+    printf "%s %s\n" $APP_NAME $APP_VERSION
+    printf "Usage: %s <sub command>\n" $APP_NAME
     printf "Sub commands:\n"
     printf "help: display this message\n"
     printf "toggle: toggle the touchpad\n"
@@ -32,53 +32,58 @@ function showHelpMessage -a appName appVersion #: string
 end
 
 # Args
-# - appName: "touchpadctl"
 # - subCommand: string
-function showInvalidSubCommandError -a appName subCommand #: string
+function showInvalidSubCommandError -a subCommand #: void
     # Easter egg!
     if [ -z $subCommand ]
         set -f subCommand "<undefined>"
     end
     printf "Invalid sub command: %s\n" $subCommand
-    printf "Use: %s help for help.\n" $appName
+    printf "Use: %s help for help.\n" $APP_NAME
+end
+
+# Args
+function showLogicalFatal #:void
+    printf "Code has a logical fatel error, some statement should never be reached, exiting!"
+    exit 2147483647
 end
 
 # Args
 # - subCommand: string
-function changeTouchpadState -a subCommand #: string
-    set -l settingPath "org.gnome.desktop.peripherals.touchpad" "send-events" #: string[2]
+function changeTouchpadState -a subCommand #: void
     switch $subCommand
         case "enable"
-            setTouchpadState $settingPath "enabled"
+            setTouchpadState "enabled"
+            sendTouchpadStateChangeDesktopNotification "<undefined>" "enabled"
         case "disable"
-            setTouchpadState $settingPath "disabled"
+            setTouchpadState "disabled"
+            sendTouchpadStateChangeDesktopNotification "<undefined>" "disabled"
         case "toggle"
-            invertTouchpadState $settingPath
+            invertTouchpadState
         case "*"
             showLogicalFatel
     end
 end
 
 # Args
-# - settingPath: string[2]
-function invertTouchpadState #: string
-    set -l settingPath $argv
-    set -l currentState (getTouchpadState $settingPath) #: "enabled" | "disabled"
+function invertTouchpadState #: void
+    set -l currentState (getTouchpadState) #: "enabled" | "disabled"
     switch $currentState
         case "enabled"
-            setTouchpadState $settingPath "disabled"
+            setTouchpadState "disabled"
+            sendTouchpadStateChangeDesktopNotification "$currentState" "disabled"
         case "disabled"
-            setTouchpadState $settingPath "enabled"
+            setTouchpadState "enabled"
+            sendTouchpadStateChangeDesktopNotification "$currentState" "enabled"
         case "*"
             showLogicalFatel
     end
 end
 
 # Args
-# - settingPath: string[2]
 function getTouchpadState #: "enabled" | "disabled"
-    set -l settingPath $argv
-    set -l rawResult (gsettings get $settingPath) #: string
+    set -l queryCommand "gsettings" "get" $SETTING_PATH #: string[3]
+    set -l rawResult (runExternalCommand $queryCommand) #: string
     set -l quoteRemovedResult (string replace "'" "" $rawResult) #:string
     # May be there's a fish's bug, we have to repeat once again to remove another quote.
     set -l reallyQuoteRemovedResult (string replace "'" "" $quoteRemovedResult) #:string
@@ -93,18 +98,51 @@ function getTouchpadState #: "enabled" | "disabled"
 end
 
 # Args
-# - settingPath: string[2]
 # - newState: "enabled" | "disabled"
-function setTouchpadState #:void
-    set -l settingPath $argv[1..2]
-    set -l newState $argv[3]
-    gsettings set $settingPath $newState
+function setTouchpadState -a newState #:void
+    set -l setCommand "gsettings" "set" $SETTING_PATH $newState #: string[4]
+    runExternalCommand $setCommand
 end
 
 # Args
-function showLogicalFatal
-    printf "Code has a logical fatel error, some statement should never be reached, exiting!"
-    exit 2147483647
+# - oldState: "enabled" | "disabled" | "<undefined>"
+# - newState: "enabled" | "disabled"
+function sendTouchpadStateChangeDesktopNotification -a oldState newState #: void
+    switch $newState
+        case "enabled"
+            set newState "ON"
+        case "disabled"
+            set newState "OFF"
+        case "*"
+            showLogicalFatal
+    end
+    set -l stateChangeText ""
+    switch $oldState
+        case "<undefined>"
+            set stateChangeText "Turned $newState unconditionally."
+        case "enabled"
+            set stateChangeText "ON → $newState"
+        case "disabled"
+            set stateChangeText "OFF → $newState"
+    end
+    sendDesktopNotification "Touchpad" "$stateChangeText"
+end
+
+# Args
+# - title: string
+# - content: string
+function sendDesktopNotification -a title content #: void
+    set -l sendNotificationCommand "notify-send" "-ea" "\"$APP_NAME\"" "\"$title\"" "\"$content\"" #: string[]
+    runExternalCommand $sendNotificationCommand
+end
+
+# Args
+# - externalCommand: string[]
+function runExternalCommand #: string
+    # Function args
+    set -l externalCommand $argv
+
+    eval $externalCommand
 end
 
 touchpadctl $argv
